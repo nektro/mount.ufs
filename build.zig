2ebc1a8bc5041b7e7d56187bc588a5965740c0e9
Meghan Denny <hello@nektro.net>
2022-08-12 16:22:49 -0700

    build.zig: add the defines

--- build.zig
+++ build.zig
@@ -9,7 +9,12 @@ pub fn build(b: *std.build.Builder) void {
     const exe = b.addExecutable("mount.ufs", "src/main.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
-    exe.addCSourceFile("src/passthrough.c", &.{});
+    exe.addCSourceFile("src/passthrough.c", &.{
+        "-DHAVE_UTIMENSAT",
+        "-DHAVE_POSIX_FALLOCATE",
+        "-DHAVE_SETXATTR",
+        "-DHAVE_COPY_FILE_RANGE",
+    });
     exe.linkLibC();
     exe.linkSystemLibrary("fuse3");
     exe.install();
